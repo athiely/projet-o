c4087ad50a4a6b15d417c35813c01edcaf765855
KevenLiving <l.kevenmedeiros.c@gmail.com>
2025-10-22 10:19:55 -0300

    Criação do banco de dados

--- backend-labkey/models.py
+++ backend-labkey/models.py
@@ -6,25 +6,50 @@ from datetime import datetime
 class Usuario(SQLModel, table=True):
     __tablename__ = "usuarios"
     id: Optional[int] = Field(default=None, primary_key=True)
-    nome: str
-    email: str
-    senha: str  
-    tipo: str = "comum"
+    nome: str = Field(index=True)
+    email: str = Field(unique=True, index=True)
+    matricula_identificacao: str = Field(unique=True, index=True)
+    senha: str 
+    tipo: str = Field(default="Aluno") 
 
-# Chave
-class Chave(SQLModel, table=True):
-    __tablename__ = "chaves"
+# Sala 
+class Sala(SQLModel, table=True):
+    __tablename__ = "salas"
     id: Optional[int] = Field(default=None, primary_key=True)
-    laboratorio: str
-    descricao: str
-    status: str
+    nome: str = Field(unique=True, index=True)
+    capacidade: int 
+    recursos: str 
 
-# Reserva
+# Reserva 
 class Reserva(SQLModel, table=True):
     __tablename__ = "reservas"
+    
     id: Optional[int] = Field(default=None, primary_key=True)
     usuario_id: int = Field(foreign_key="usuarios.id")
-    chave_id: int = Field(foreign_key="chaves.id")
-    inicio: datetime
-    fim: datetime
-    status: str = "ativa"
+    sala_id: int = Field(foreign_key="salas.id")
+    inicio: datetime = Field(index=True)
+    fim: datetime = Field(index=True)
+    status: str = Field(default="Pendente", index=True)
+    periodicidade: Optional[str] = Field(default=None) 
+
+
+# Notificação 
+class Notificacao(SQLModel, table=True):
+    __tablename__ = "notificacoes"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    usuario_id: int = Field(foreign_key="usuarios.id")
+    reserva_id: Optional[int] = Field(default=None, foreign_key="reservas.id")
+    mensagem: str
+    data_envio: datetime = Field(default_factory=datetime.utcnow)
+    lida: bool = Field(default=False)
+
+# Log Auditoria
+class LogAuditoria(SQLModel, table=True):  
+    __tablename__ = "logs_auditoria"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    usuario_id: Optional[int] = Field(default=None, foreign_key="usuarios.id")
+    acao: str
+    timestamp: datetime = Field(default_factory=datetime.utcnow)
+    detalhes: Optional[str] = Field(default=None)
+
+
